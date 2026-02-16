43396b751cc085d943c2b095ec738842b0704c2f
alejandromedina55-lab <alejandro.medina55@uabc.edu.mx>
2026-02-15 17:19:08 -0800

    Create Problema4.c

---
+++ Problema4.c
@@ -0,0 +1,34 @@
+//
+// Created by Alexm on 09/02/2026.
+//
+    printf("3) Calcular la raiz cuadrada de un número\n");
+    printf("4) Terminar el programa\n\n");
+    printf("Inserte el número de la cosa a realizar: ");
+    scanf("%d", &eleccion);
+
+#include <stdio.h>
+
+int main() {
+    int eleccion;
+    printf("Escoja una de estas opciones.\n");
+    printf("1) Determinar si el numero es par o impar\n");
+    printf("2) Elevar un numero a una potencia\n");
+
+    switch(eleccion){
+        case 1:
+            printf("\nPara saber si un numero es impar o par, debes de hacer la operación [variable]%2, si el resultado te da 0 entonces es par, sino entonces no lo es.");
+            break;
+        case 2:
+            printf("La operación que se ocupa para esto es [variable] * [variable], o bien [variable] * n donde n es igual a otro número dado por el usuario o por el programa.");
+            break;
+        case 3:
+            printf("La operación que se ocupa añadir la librería <math.h>, utilizando en, por ejemplo raiz=sqrt([variable]) el como sacar la raíz.");
+            break;
+        case 4:
+            printf("Esta opción terminaría el programa. Presuntamente esto se usaría en un ciclo en donde la única forma de detenerlo sería escoger esta opción.");
+            break;
+        default:
+            printf("Esto no es una opción valida.");
+    }
+    return 0;
+}
